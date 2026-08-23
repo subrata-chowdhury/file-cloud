af98e6abca79eb75c98f6acc2321f3541879471c
Subrata Chowdhury <subratachowdhurysuper@gmail.com>
2026-08-23 23:03:27 +0530

    feat: update dashboard layout for mobile responsiveness

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,17 @@
 import DashboardNav from './components/DashboardNav';
 import Sidebar from './components/Sidebar';
+import { MobileMenuProvider } from './context/MobileMenuContext';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-gray-50/50">
-      <Sidebar />
-      <div className="flex w-full flex-col pl-64 transition-all">
-        <DashboardNav />
-        <div className="flex-1 overflow-x-hidden">{children}</div>
+    <MobileMenuProvider>
+      <div className="flex min-h-screen bg-gray-50/50">
+        <Sidebar />
+        <div className="flex w-full flex-col transition-all lg:pl-64">
+          <DashboardNav />
+          <div className="flex-1 overflow-x-hidden">{children}</div>
+        </div>
       </div>
-    </div>
+    </MobileMenuProvider>
   );
 }
